5387c2e9c01d0daf042d1e70edb0fe222b30217d
Chase Darnall <emailchased@gmail.com>
2024-05-13 00:06:05 -0500

    finished functional componenets and tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,33 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+import React from "react";
+import axios from "axios";
+import AppFunctional from "./AppFunctional";
+import {render, fireEvent, screen, waitFor} from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('AppFunctional Component', ()=> {
+  test('Initial Rendering Test', () => {
+    render(<AppFunctional />)
+    expect(screen.getByText('Coordinates (2, 2)')).toBeInTheDocument()
+    expect(screen.getByText('You moved 0 times')).toBeInTheDocument()
+    expect(screen.getByText('B')).toBeInTheDocument()
+  })
+  test('Reset Function Test', () => {
+    render(<AppFunctional />)
+    fireEvent.click(up);
+    fireEvent.click(reset)
+    expect(screen.getByText('Coordinates (2, 2)')).toBeInTheDocument()
+    expect(screen.getByText('You moved 0 times')).toBeInTheDocument()
+  })
+  test('Move UP Function Test', () => {
+    render(<AppFunctional />)
+    fireEvent.click(up);
+    expect(screen.getByText('Coordinates (2, 1)')).toBeInTheDocument()
+    expect(screen.getByText('You moved 1 time')).toBeInTheDocument()
+  })
+  test ('Move DOWN Function Test', () => {
+    render(<AppFunctional />)
+    fireEvent.click(down);
+    expect(screen.getByText('Coordinates (2, 3)')).toBeInTheDocument()
+  })
 })
